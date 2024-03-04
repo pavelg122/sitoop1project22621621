713c4c96e2abac80c5a48848d73173d58cd4aaee
Pavel Georgiev <pavel.georgiev122@gmail.com>
2024-03-04 20:44:25 +0200

    test commit

---
+++ sitoop1project22621621/src/Application.java
@@ -0,0 +1,8 @@
+
+public class Application {
+    public static void main(String[] args) {
+        FileHandlerImpl fileHandler= new FileHandlerImpl();
+        fileHandler.help();
+        fileHandler.exit();
+    }
+}

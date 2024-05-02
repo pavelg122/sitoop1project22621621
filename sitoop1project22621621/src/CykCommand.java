0444bffd9fbf312fe58266ff6fe34c4dbb88e27b
Pavel Georgiev <pavel.georgiev122@gmail.com>
2024-05-02 21:42:28 +0300

    more cyk work..

--- sitoop1project22621621/src/CykCommand.java
+++ sitoop1project22621621/src/CykCommand.java
@@ -20,8 +20,16 @@ public class CykCommand implements Command{
             uniqueTerminals.addAll(rule.getTerminals());
             uniqueNonterminals.add(rule.getNonterminals());
         }
+        /*ArrayList<Rule> unitProductions = new ArrayList<>();
+        ArrayList<Rule> nonUnitProductions = new ArrayList<>();*/
         ArrayList<String> terminalsUnique = new ArrayList<>(uniqueTerminals);
         ArrayList<String> nonterminalsUnique = new ArrayList<>(uniqueNonterminals);
+        String[] nonterminalsArr = new String[grammar.getRules().size()];
+        int ii=0;
+        for(Rule rule: grammar.getRules()){
+            nonterminalsArr[ii] = rule.getNonterminals();
+            ii++;
+        }
         Iterator<Rule> iterator1 = grammar.getRules().iterator();
         List<String> wordList = List.of(word.split(""));
         Rule retrieved1 = null;
@@ -32,7 +40,23 @@ public class CykCommand implements Command{
         String start = retrieved1.getNonterminals();
         int r = uniqueNonterminals.size();
         int n = word.length();
-        /*boolean[][][] P = new boolean[n][n][r];
+        /*for(Rule rule: grammar.getRules()){
+                for(String terminal: rule.getTerminals()){
+                    if(terminal.length() == 1){
+                        ArrayList<String> terminalArrList = new ArrayList<>();
+                        terminalArrList.add(terminal);
+                        unitProductions.add(new Rule(rule.getNonterminals(), terminalArrList));
+                    }
+                    if(terminal.length() == 2){
+                        char[] terminalCharArr = terminal.toCharArray();
+                        ArrayList<String> terminalArrList1 = new ArrayList<>();
+                        terminalArrList1.add(String.valueOf(terminalCharArr[0]));
+                        terminalArrList1.add(String.valueOf(terminalCharArr[1]));
+                        nonUnitProductions.add(new Rule(rule.getNonterminals(), terminalArrList1));
+                    }
+                }
+        }*/
+        boolean[][][] P = new boolean[n][n][r];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 for(int k=0;k<r;k++){
@@ -40,8 +64,45 @@ public class CykCommand implements Command{
                 }
             }
         }
+        ArrayList<Triple<Integer,Integer,Integer>>[][][] back = new ArrayList[n][n][r];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<r;k++){
+                    back[i][j][k] = new ArrayList<>();
+                }
+            }
+        }
+        for(int s=1;s<=n;s++){
+            for(int v=1;v<=r;v++)
+                if(isUnitProduction(nonterminalsArr[v-1], word.charAt(s-1),grammar.getRules())){
+                P[1][s][v]=true;}
+            }
 
-        */
+        for(int l=2;l<=n;l++){
+            for(int s=1;s<=n-l+1;s++){
+                for(int p=1;p<=l-1;p++){
+                    for(int a=0;a<r;a++){
+                        for(int b=0;b<r;b++){
+                            for(int c=0;c<r;c++){
+                        if(P[p][s][b]&&P[l-p][s+p][c]){
+                            P[l][s][a] = true;
+                            back[l][s][a].add(new Triple<>(p,b,c));
+                        }
+                    }
+                        }
+                    }
+
+
+                }
+            }
+        }
+        if(P[n-1][1][1]){System.out.println("The word " + word +
+                " is in the language of grammar: "
+                + grammar.getId());}
+        else {System.out.println("The word " + word +
+                " is NOT in the language of grammar: "
+                + grammar.getId());}
+        /*
         Map<Integer,Map<Integer,Set<String>>> T = new HashMap<>();
         for(int j=0;j<n;j++){
             for(Rule rule:grammar.getRules()){
@@ -79,14 +140,49 @@ public class CykCommand implements Command{
                     }
                 }
             }
-        }
-        if(T.get(0)!= null && T.get(0).get(n-1) != null
+        }*/
+        
+        /*if(T.get(0)!= null && T.get(0).get(n-1) != null
         && T.get(0).get(n-1).size() != 0)
             System.out.println("The word " + word +
                     " is in the language of grammar: "
                     + grammar.getId());
         else System.out.println("The word " + word +
                 " is NOT in the language of grammar: "
-                + grammar.getId());
+                + grammar.getId());*/
     }
+    public class Triple<L,M,R>{
+        private final L left;
+        private final M middle;
+        private final R right;
+
+        public Triple(L left, M middle, R right) {
+            this.left = left;
+            this.middle = middle;
+            this.right = right;
+        }
+
+        public L getLeft() {
+            return left;
+        }
+
+        public M getMiddle() {
+            return middle;
+        }
+
+        public R getRight() {
+            return right;
+        }
+    }
+  private boolean isUnitProduction(String nonterminal,char terminal,Set<Rule> rules){
+for(Rule rule:rules){
+    for(String terminal1: rule.getTerminals()){
+        if(rule.getNonterminals().equals(nonterminal)&&terminal1.equals(String.valueOf(terminal))){
+            return true;
+        }
+    }
+
+}
+        return false;
+  }
 }
